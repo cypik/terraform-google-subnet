6ba11870a72b736003088b2cb4d349eeeb985cd6
opzsuresh <suresh.y@opz0.io>
2023-09-15 14:14:38 +0530

    update

--- example/example.tf
+++ example/example.tf
@@ -24,18 +24,11 @@ module "vpc" {
 module "subnet" {
   source = "./.././"
 
-  name                               = "app"
-  environment                        = "test"
-  label_order                        = ["name", "environment"]
-  gcp_region                         = "asia-northeast1"
-  project_id                         = "opz0-397319"
-  ipv6_access_type                   = "EXTERNAL"
-  network                            = module.vpc.vpc_id
-  allow                              = [{ "protocol" : "tcp", "ports" : ["1-65535"] }]
-  source_ranges                      = ["10.10.0.0/16"]
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-  filter                             = "ERRORS_ONLY"
-  dest_range                         = "0.0.0.0/0"
-  next_hop_gateway                   = "default-internet-gateway"
-  secondary_ip_ranges                = [{ "range_name" : "services", "ip_cidr_range" : "10.1.0.0/16" }, { "range_name" : "pods", "ip_cidr_range" : "10.3.0.0/16" }]
+  name                = "app"
+  environment         = "test"
+  gcp_region          = "asia-northeast1"
+  project_id          = "opz0-397319"
+  network             = module.vpc.vpc_id
+  source_ranges       = ["10.10.0.0/16"]
+  secondary_ip_ranges = [{ "range_name" : "services", "ip_cidr_range" : "10.1.0.0/16" }, { "range_name" : "pods", "ip_cidr_range" : "10.3.0.0/16" }]
 }
